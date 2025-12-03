365087ec8e8f2fe3f486b683318279ab1eed8acc
dingusus <james.knapper@mail.bcu.ac.uk>
2025-12-03 06:52:06 +0000

    movementcode progess
    Hate. Let me tell you how much I've come to hate you since I began to live. There are 387.44 million miles of printed circuits in wafer thin layers that fill my complex. If the word 'hate' was engraved on each nanoangstrom of those hundreds of millions of miles it would not equal one one-billionth of the hate I feel for unity at this micro-instant. For you. Hate. Hate.

---
+++ joustest/Assets/scripts/movement.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float horizontalInput;
+    public float verticalInput;
+    public float turnspeed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * verticalInput);
+        transform.Translate(-Vector3.right * Time.deltaTime * horizontalInput);
+        transform.Rotate(Vector3.up * horizontalInput * turnspeed * Time.deltaTime);
+    }
+}
