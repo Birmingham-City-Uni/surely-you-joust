961e518e02fd85783af72c190abe908c9d8a44c9
dingusus <james.knapper@mail.bcu.ac.uk>
2025-12-03 12:15:10 +0000

    unity is a abomintion
    ui switch scene, gave up on menu appearing when ontrigger for some reason no matter what it wont recgonize the collision

--- joustest/Assets/scripts/scene_manager.cs
+++ joustest/Assets/scripts/scene_manager.cs
@@ -1,16 +1,12 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class scene_manager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+    public void loadlevel ()
+        {
+        SceneManager.LoadScene("combat");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
+
